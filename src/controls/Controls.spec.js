a3695a0e0a5247035f5b1ccba575b0ef63b8d6a4
Jacob Washburn <jacobrwashburn@gmail.com>
2019-12-16 17:05:10 -0700

    some testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,12 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+
+import Controls from './Controls';
+
+
+test('controls renders without crashing', () => {
+    render(<Controls/>);
+});
+
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+// Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+
+import Display from './Display';
+
+
+test('controls renders without crashing', () => {
+    render(<Display/>);
+});
